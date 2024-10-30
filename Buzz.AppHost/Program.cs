b3bb0b1a58b0914eb609a4e4e9187c99ddca1f70
Qin Guan <qinguan20040914@gmail.com>
2024-10-31 00:12:46 +0800

    Update launchSettings

--- Buzz.AppHost/Program.cs
+++ Buzz.AppHost/Program.cs
@@ -3,6 +3,7 @@ using Projects;
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Buzz_WebApi>("api");
-builder.AddNpmApp("app", "../Buzz.WebApp", "dev");
+builder.AddNpmApp("app", "../Buzz.WebApp", "dev")
+    .WithHttpEndpoint(targetPort: 3000);
 
 builder.Build().Run();
