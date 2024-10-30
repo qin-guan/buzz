d0a4e2d9d3fc4376e1a74be26b2b06bca7c8a93d
Qin Guan <qinguan20040914@gmail.com>
2024-10-31 00:08:54 +0800

    Update WebApp configuration

--- Buzz.AppHost/Program.cs
+++ Buzz.AppHost/Program.cs
@@ -3,5 +3,6 @@ using Projects;
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Buzz_WebApi>("api");
+builder.AddNpmApp("app", "../Buzz.WebApp", "dev");
 
 builder.Build().Run();
--- Buzz.WebApp/nuxt.config.ts
+++ Buzz.WebApp/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: "2024-10-31"
 })
